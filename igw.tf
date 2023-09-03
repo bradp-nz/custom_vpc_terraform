c2fd9d91ed4f2ea783f7b8e25240a8815af2b16e
brandon <112357095+bradp-nz@users.noreply.github.com>
2023-09-04 10:29:53 +1200

    fixed igw issue

--- igw.tf
+++ igw.tf
@@ -1,6 +1,3 @@
-resource "aws_internet_gateway_attachment" "a4l-vpc1-igw" {
-  internet_gateway_id = aws_internet_gateway.a4l-vpc1-igw.id
+resource "aws_internet_gateway" "a4l-vpc1-igw" {
   vpc_id              = aws_vpc.a4l-vpc1.id
 }
-
-resource "aws_internet_gateway" "a4l-vpc1-igw" {}
